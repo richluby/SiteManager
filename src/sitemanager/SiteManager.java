527ee57c51fbc75f6ad40b0b0149d04149367d2a
richluby <richluby@gmail.com>
2015-04-01 14:06:59 -0500

    initial commit Signed-off-by:richluby <richluby@gmail.com>

---
+++ src/sitemanager/SiteManager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sitemanager;
+
+/**
+ * This application eases the management of personnel accounts and certificates with
+ * relation to a custom deployment environment.
+ * @author Richard Luby
+ */
+public class SiteManager {
+
+	/**
+	 * the entry point for the application
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+
+}
