32b6cf7e9c405d07d179b13f1905ede0ca026f12
Richard Luby <richluby@gmail.com>
2015-05-12 18:56:33 -0500

    added model to implement RPN math updated template accordingly

---
+++ src/sitemanager/RPNModel.java
@@ -0,0 +1,100 @@
+import java.io.IOException;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+/**@author Richard Luby, Copyright 2013*/
+/** model and processor for the data contained in the program */
+public class RPNModel {
+	/** special commands for the program */
+	final public static class COMMANDS {
+		/** clear the entire stack */
+		final public static char CLEAR_STACK = 'c';
+		/** swap the top two elements of the stack */
+		final public static char SWAP_TOP_STACK = 's';
+	}
+	/** the stack for the program */
+	private Deque<Double> stack;
+	/** creates and returns a basic dataModel */
+	public RPNModel() {
+		stack = new LinkedList<Double>();
+	}
+	/** parses an action based on the top two elements in the stack, and on the
+	 * operator/command given
+	 * @param operator the operator to use on the top two stack items
+	 * @return returns the result of an arithmetic operation
+	 * @throws NoSuchElementException thrown when there are not enough items in
+	 *             the stack
+	 * @throws IOException thrown when the swapping operation has completed
+	 *             successfully to provide notification, or when the stack has
+	 *             been cleared */
+	public double parseAction(char operator) throws NoSuchElementException, IOException{
+		double result = 0;
+		if (operator == COMMANDS.CLEAR_STACK) {
+			stack.clear();
+			throw new IOException("Clearing stack...");
+		}
+		if (!stack.isEmpty()) {
+			double a = stack.pop(), b = 0;
+			if (!stack.isEmpty()) {
+				b = stack.pop();//NOTE: this is used to ensure that there can be both items before popping 'a' off the stack
+			} else {
+				stack.push(a);//replace 'a' in the stack
+				throw new NoSuchElementException("Not enough elements in stack");
+			}
+			switch (operator) {
+				case '+' :
+					result = b + a;
+					stack.push(result);
+					break;
+				case '-' :
+					result = b - a;
+					stack.push(result);
+					break;
+				case '*' :
+					result = b * a;
+					stack.push(result);
+					break;
+				case '/' :
+					result = b / a;
+					stack.push(result);
+					break;
+				case 'M' ://mod the top two items
+					result = b % a;
+					stack.push(result);
+					break;
+				case COMMANDS.SWAP_TOP_STACK ://swap top two stacks
+					result = b;
+					stack.push(a);
+					stack.push(result);
+					throw new IOException("Swapping");
+					//break;
+			}
+		} else {
+			throw new IOException("Empty Stack");
+		}
+		return result;
+	}
+	/** pushes a number onto the stack
+	 * @param num the number to push onto the stack */
+	public void push(double num){
+		stack.push(num);
+	}
+	/** returns the top two items on the stack
+	 * @return returns top two items on the stack */
+	public double[] getTopTwoStackElements(){
+		if (!stack.isEmpty()) {
+			double a = stack.pop(), b = -1;
+			if (stack.isEmpty()) {
+				stack.push(a);
+				return null;
+			}
+			b = stack.pop();
+			stack.push(b);
+			stack.push(a);
+			return new double[]{a,b};
+		} else {
+			return null;
+		}
+	}
+}
